cba0fb1f8ff2e2add457313481729bf0b13a3cb6
Matthew Lollar <mjlollar1@gmail.com>
2024-04-22 16:47:56 -0500

    Create get_sync_depth.py

---
+++ get_sync_depth.py
@@ -0,0 +1,24 @@
+#Average Depth of sync file
+#expected output, one text file with a single chrom,match,mismatch row
+#MJL 04/22/24
+
+import argparse
+import numpy as np
+import pandas as pd
+import sys
+import os.path
+import csv
+
+parser = argparse.ArgumentParser(description="Compare to sync files")
+parser.add_argument('--s', help='Sync File', required=True, type=str)
+args = parser.parse_args()
+
+### Load sync file and assign column names
+df = pd.read_csv(args.s, sep='\t', header=None)
+df.columns = ['chr', 'pos', 'refA', 'countA']
+df[['A', 'T', 'C', 'G', 'countN1', 'countDel1']] = df['countA'].str.split(':', expand=True)
+
+read_list = ['A', 'T', 'C', 'G']
+df['depth'] = df[read_list].astype(int).sum(axis=1)
+final_depth = (df['depth'].sum()/(df.shape[0]-1))
+print(str(final_depth))
